411ea2a3da0c680865b06a83b1db876111d1763b
Rupert Broad <rupert.broad@gmail.com>
2019-02-24 17:38:21 +0100

    add outputs

--- ssm/outputs.tf
+++ ssm/outputs.tf
@@ -1,2 +1,17 @@
 # Outputs
 
+output "rds_db_name" {
+  value = "${aws_ssm_parameter.rds_db_database.name}"
+}
+
+output "rds_db_endpoint" {
+  value = "${aws_ssm_parameter.rds_db_endpoint.name}"
+}
+
+output "cs_api_db_user_name" {
+  value = "${aws_ssm_parameter.user_name.name}"
+}
+
+output "cs_api_db_user_pass" {
+  value = "${aws_ssm_parameter.user_password.name}"
+}
